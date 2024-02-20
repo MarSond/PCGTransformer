514f66779e0768564027487940220af91aca2f2a
Martin <martin.sondermann004@stud.fh-dortmund.de>
2024-02-20 17:45:07 +0100

    Dataset 2016 parse

--- data/dataset.py
+++ data/dataset.py
@@ -1,7 +1,7 @@
 import os
 from os.path import join as pjoin
 from os.path import normpath
-
+import pandas as pd
 
 class Dataset:
 
@@ -14,10 +14,10 @@ class Physionet2016(Dataset):
 	def __init__(self):
 		super().__init__()
 		self.folder_name = 'physionet2016'
-		self.meta_file_train = 'train_list.csv'
-		self.meta_file_test = 'test_list.csv'
 		self.dataset_path = pjoin(self.base_path, self.folder_name)
-		self.train_audio_path = f"{self.dataset_path}audiofiles/train/*/*.wav"
+		self.meta_file_train = pjoin(self.dataset_path, 'train_list.csv')
+		self.meta_file_test = pjoin(self.dataset_path, 'test_list.csv')
+		self.train_audio_path = f"{self.dataset_path}/audiofiles/train/*/*.wav"
 		self.num_classes = 2
 
 	
@@ -30,8 +30,53 @@ class Physionet2022(Dataset):
 		self.meta_file_train = 'train_list.csv'
 		self.meta_file_test = 'test_list.csv'
 		self.dataset_path = pjoin(self.base_path, self.folder_name)
-		self.train_audio_path = f"{self.dataset_path}audiofiles/train/*/*.wav"
+		self.train_audio_path = f"{self.dataset_path}/audiofiles/train/*/*.wav"
 		self.num_classes = 2
+
+def get_file_metadata_human(path: str, anno: pd.DataFrame, data_classes: pd.DataFrame, dataset_object: Physionet2016):
+	meta = { }
+	id = Path(path).name.split(".")[0]
+	dataset_name = str(Path(path).parents[0]).split("\\")[-1]
+	info = torchaudio.info(path)
+	audio = torchaudio.load(path)
+	
+	try: quality = data_classes.loc[id]['quality'].squeeze()
+	except KeyError:
+		quality = 1
+	if np.isnan(quality):
+		quality = 1	# Assume quality is okay if no data is given
+	
+	######### Get diagnosis #########
+	try: diagnosis = anno.loc[id]['Diagnosis']
+	except KeyError:
+		# diagnosis not found - likely id not in annotation list - use old name from other list
+		new_id = anno['Original record name'] == id
+		val = anno.loc[new_id]["Diagnosis"].squeeze()
+		if len(val) == 0:  
+			val = "Unknown"
+		diagnosis = str(val)
+
+	meta["path"] = path.replace(dataset_object.dataset_path + "\\","")
+	
+	#meta["path"] = path.replace(paths['train_audio_path'].replace("/","\\") + "\\","")
+	meta["name"] = id
+	meta["dataset"] = dataset_name
+	meta['diagnosis'] = diagnosis	# NOT the label, but the diagnosis string
+	meta["quality"] = int(quality)
+	meta["sr"] = info.sample_rate 
+	meta["channels"] = info.num_channels 
+	meta["length"] = info.num_frames
+	meta["bits"] = info.bits_per_sample 
+	#meta["min_amp"] = audio[0].min().item()
+	#meta["max_amp"] = audio[0].max().item()
+	try:    # Try "updated" annotation list
+		dataclass = int(anno.loc[id]['Class (-1=normal 1=abnormal)'].squeeze())
+	except KeyError:    # If fails, use original dataset annotation
+		dataclass = int(data_classes.loc[id]['class'].squeeze())
+	if dataclass == -1:
+		dataclass = 0
+	meta["label_1"] = dataclass
+	return meta
 
 def parse_physionet2016():
 	"""
@@ -95,6 +140,21 @@ def parse_physionet2016():
 	print("All train data count", len(train_data))
 	print("All annotations count", len(anno))
 	print("All classes count", len(data_classes))
+	metadata = []
+
+	pbar = tqdm.tqdm(total=len(train_data), position=0, leave=True, desc="Human data list")
+	for file in train_data:
+		metadata.append(get_file_metadata_human(file, anno, data_classes, dataset))
+		pbar.update(1)
+
+		
+
+	metadata_df = pd.DataFrame(metadata)
+	print(metadata_df.head())
+	print("Train path", dataset.meta_file_train)
+	metadata_df.to_csv(dataset.meta_file_train, index=True, index_label="id", encoding='utf-8')
+	
+
 
 if __name__ == '__main__':
 	# parse folder structure and create meta files
